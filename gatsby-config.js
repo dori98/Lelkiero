f4acb72775e3b4dcad71b9afb59a9adb46970e14
Káposzta Gergő <gergo.kaposzta@kifli.tech>
2021-07-18 14:56:28 +0200

    ajanlott irodalom update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,20 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-contentful`,
+
       options: {
         spaceId: `zvoljr72g0f1`,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken:  `TekK8wUlNUNpYEVSJiqvsHb4IRtk3ll6-a-yK0ZTvSk`,
       },
 
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
     },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
--- src/pages/AjanlottIrodalom.js
+++ src/pages/AjanlottIrodalom.js
@@ -1,12 +1,25 @@
 import React from "react"
 import * as styles from "../css/style.module.css"
 import {graphql, useStaticQuery} from "gatsby";
+import Img from "gatsby-image"
 import * as style from "../css/style.module.css";
 
 const Ajanlott = () => {
     const data = useStaticQuery(graphql`
    {
-    allContentfulAjanlottIrodalom(sort: {fields: cim, order: ASC}) {
+    konyv: allContentfulAjanlottIrodalom(filter: {weboldal: {eq: false}}) {
+      nodes {
+        id
+        cim
+        konyvKepe {
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+    },
+    
+    weboldal: allContentfulAjanlottIrodalom(filter: {weboldal: {eq: true}}) {
       nodes {
         id
         cim
@@ -16,20 +29,45 @@ const Ajanlott = () => {
   }
   `)
 
-    const{
-        allContentfulAjanlottIrodalom:{nodes:ajanlott}
-    }=data
-    return(
-        <>
+    const {
+        konyv: {nodes: konyv},
+        weboldal: {nodes: weboldal}
+    } = data
 
-            <div className={style.ajanlottFullPage}>
-                <h1 className={styles.indexH1}>Ajánlott irodalmak</h1>
-                <h3 className={style.arakH3}>Bla Bla Bla de kell valami </h3>
+    return (
+        <div className={style.ajanlottFullPage}>
+            <h1 className={styles.indexH1}>Ajánlott irodalmak</h1>
+            <h3 className={style.arakH3}>Bla Bla Bla de kell valami </h3>
+            <h2>Könyvek</h2>
+            <div className={styles.bookDiv}>
 
-                <p >Könyvek</p>
-                <p>Weboldalak</p>
+                {konyv.map(konyv => {
+                    return (
+                        <div>
+                            <div key={konyv.id}>
+                                <Img fluid={konyv.konyvKepe.fluid} className={styles.imageSize}/>
+                                <p>{konyv.cim}</p>
+                            </div>
+                        </div>
+
+                    )
+                })}
             </div>
-        </>
+
+            <h2>Weboldalak</h2>
+            <div>
+
+                {weboldal.map(weboldal => {
+                    return (
+                        <div>
+                            <div key={weboldal.id}>
+                                <a href={weboldal.link}>{weboldal.cim}</a>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 export default Ajanlott
